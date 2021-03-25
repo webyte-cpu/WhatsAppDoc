812aa335a354e0f452f1140982db3f906b5d295f
raimieltan <raimieltan@gmail.com>
2021-03-25 17:55:51 +0800

    added a route to change pending requests to verified status

--- backend/routes/viewDoctors.js
+++ backend/routes/viewDoctors.js
@@ -16,12 +16,10 @@ router.get("/verified", async (req, res) => {
     }
 })
 
-
-
-router.get("/pending", async (req, res) => {
+router.get("/verified", async (req, res) => {
     try {
      
-        const doctors = await pool.query('SELECT * from doctors where verified = False')
+        const doctors = await pool.query('SELECT * from doctors where verified = True')
 
         res.json(doctors.rows)
 
@@ -31,4 +29,23 @@ router.get("/pending", async (req, res) => {
         res.status(500).json(error.message)
     }
 })
+
+
+
+router.patch("/:id", async (req, res) => {
+    try {
+        
+        const { id } = req.params
+        const doctors = await pool.query('UPDATE doctors SET verified = True WHERE id = $1 RETURNING *' , [id])
+
+        res.json(doctors.rows[0])
+
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json(error.message)
+    }
+})
+
+
 module.exports = router;
--- frontend/README.md
+++ frontend/README.md
@@ -1,1 +1,5 @@
-# WhatsAppDoc
+Backend-
+
+http://localhost:8000/doctors/verified -- returns all verified doctors
+http://localhost:8000/doctors/pending -- returns all pending doctors
+http://localhost:8000/doctors/id (note id is the id of the doctor in the database) -- change pending to verified
