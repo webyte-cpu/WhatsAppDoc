62a700dfc58d78cf2d591d468db0560f2f7b7b90
edelynnm <edelynnm@gmail.com>
2021-03-26 15:50:55 +0800

    feat: add web url linking

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
 import { routes } from './app-routes';
 
 const { Navigator, Screen } = createStackNavigator();
@@ -10,10 +12,22 @@ const navigationRoutes = (routes) => {
   ))
 }
 
+const linking = {
+  config: {
+    Home: "home",
+    Login: "",
+    ForgotPass: "forgotpass",
+  }
+}
+
 const AppNavigator = (props) => (
-  <Navigator {...props} headerMode="none">
-    { navigationRoutes(routes) }
-  </Navigator>
+  <SafeAreaProvider>
+    <NavigationContainer linking={linking}>
+      <Navigator {...props} headerMode="none">
+        { navigationRoutes(routes) }
+      </Navigator>
+    </NavigationContainer>
+  </SafeAreaProvider>
 );
 
 export default AppNavigator;
