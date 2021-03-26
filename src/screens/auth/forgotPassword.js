e54472573eb2c993792ca5a2360a894f307df8c5
edelynnm <edelynnm@gmail.com>
2021-03-26 15:55:00 +0800

    feat: add initial forgotPass Page UI

--- src/screens/auth/forgotPassword.js
+++ src/screens/auth/forgotPassword.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-import { Button, Layout, Text, Input, Icon, Card } from '@ui-kitten/components';
-import { AppRoute } from '../../navigation/app-routes';
+import React, { useState } from "react";
+import { View, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import {
+  Button,
+  Text,
+  Input,
+  Icon,
+  TopNavigation,
+  TopNavigationAction,
+  Divider,
+} from "@ui-kitten/components";
+import { AppRoute } from "../../navigation/app-routes";
+import Template from "../template";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 const ForgotPassword = ({ navigation }) => {
-  const logout = () => navigation.navigate(AppRoute.LOGIN.name)
+  const [secureTextEntry, setSecureTextEntry] = useState(true);
+  const [inputEmail, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const toggleSecureEntry = () => setSecureTextEntry(!secureTextEntry);
+
+  const showPasswordIcon = (props) => (
+    <TouchableWithoutFeedback onPress={toggleSecureEntry}>
+      <Icon {...props} name={!secureTextEntry ? "eye" : "eye-off"} />
+    </TouchableWithoutFeedback>
+  );
+
+  const backBtn = (props) => <Icon {...props} name="arrow-back" />;
+
+  const backAction = () => (
+    <TopNavigationAction icon={backBtn} onPress={() => navigation.goBack()} />
+  );
+
+  const sendLink = () => navigation.navigate(AppRoute.LOGIN.name);
+
+  const forgotPassPage = (
+    <>
+      <TopNavigation accessoryLeft={backAction} title="Forgot Password" />
+      <Divider />
+      <View style={{ padding: 30 }}>
+        <Input
+          testID="email"
+          label="Email"
+          selectTextOnFocus
+          placeholder="Enter email"
+          value={inputEmail}
+          onChangeText={setEmail}
+          style={{ paddingBottom: 10 }}
+        />
+        <Button onPress={sendLink}>Send Link</Button>
+      </View>
+    </>
+  );
   return (
-    <SafeAreaView>
-      <Text>Forgot Password</Text>
-      <Button onPress={logout}>Logout</Button>
-    </SafeAreaView>
-  )
-}
+    <>
+      <Template
+        backgroundColor="white"
+        contentContainerStyle={styles.container}
+        children={forgotPassPage}
+      />
+    </>
+  );
+};
 
 export default ForgotPassword;
