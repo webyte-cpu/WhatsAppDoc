c5ab0b862f775926a1257c36808531eb9fe93b68
edelynnm <edelynnm@gmail.com>
2021-03-27 00:10:34 +0800

    refactor: change template to cater scrollView

--- src/screens/template.js
+++ src/screens/template.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import { View } from 'react-native';
-import { SafeAreaView } from "react-native-safe-area-context";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { ScrollView, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
-const Template = ({ backgroundColor, contentContainerStyle, children}) => {
+const withoutScrollView = (children, contentContainerStyle, bgColor) => {
+  const mergedStyles = {...contentContainerStyle, backgroundColor: bgColor, flex: 1 }
+  
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor }}>
-      <KeyboardAwareScrollView contentContainerStyle={contentContainerStyle}>
-        <View>
-          {children}
-        </View>
-      </KeyboardAwareScrollView>
+  <KeyboardAwareScrollView
+    contentContainerStyle={mergedStyles}
+  >
+    <View>{children}</View>
+  </KeyboardAwareScrollView>
+)};
+
+const withScrollView = (children, contentContainerStyle, bgColor) => (
+  <ScrollView>
+    {withoutScrollView(children, contentContainerStyle, bgColor)}
+  </ScrollView>
+);
+
+const Template = ({
+  isScrollable = false,
+  bgColor = 'white',
+  contentContainerStyle = {},
+  children,
+}) => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      {isScrollable
+        ? withScrollView(children, contentContainerStyle, bgColor)
+        : withoutScrollView(children, contentContainerStyle, bgColor)}
     </SafeAreaView>
-  )
-}
+  );
+};
 
 export default Template;
