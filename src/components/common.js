2c1a722e29a9c8b2f3972b1eefd69b2506e76237
edelynnm <edelynnm@gmail.com>
2021-03-28 21:00:26 +0800

    refactor: add drawer option to topHeaderView

--- src/components/common.js
+++ src/components/common.js
@@ -7,16 +7,23 @@ import {
   Text
 } from '@ui-kitten/components';
 
-const TopHeaderView = ({title, component, titleColor, btnColor, topbarColor = 'transparent', backTo}) => {
+const TopHeaderView = ({title, component, titleColor, btnColor, topbarColor = 'transparent', backTo = null, hasDrawer = false }) => {
   const backBtn = (btnColor = "#222b45", props) => <Icon {...props} name='arrow-back' fill={btnColor}/>;
 
   const backAction = (btnColor, backTo) => (
     <TopNavigationAction icon={(props) => backBtn(btnColor, props)} onPress={() => backTo()} />
   );
+
+  const drawer = 'something'
+
+  const leftIcon = (hasDrawer, btnColor, backTo) => {
+    return hasDrawer ? drawer : backAction(btnColor, backTo)
+  }
+
   return (
   <View>
     <TopNavigation
-      accessoryLeft={() => backAction(btnColor, backTo)}
+      accessoryLeft={() => leftIcon(hasDrawer, btnColor, backTo)}
       style={{ backgroundColor: { topbarColor } }}
     />
     <View>
