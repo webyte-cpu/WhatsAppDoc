2d66f7839b52e336d68bfd6674efd4589617d0fb
edelynnm <edelynnm@gmail.com>
2021-03-31 15:38:48 +0800

    refactor: route

--- src/navigation/app-routes.js
+++ src/navigation/app-routes.js
@@ -1,32 +1,13 @@
 /**
- * Add routes here
+ * Define component of route in src/navigation/routes
  */
 
-import LoginScreen from '../screens/auth/login'
-import ForgotPassword from '../screens/auth/forgotPassword'
-import HomeScreen from '../screens/home/home'
-import SignupScreen from '../screens/auth/signup'
-
 const AppRoute = { 
-  LOGIN: {
-    name: 'Login',
-    component: LoginScreen
-  },
-  SIGNUP: {
-    name: 'Signup',
-    component: SignupScreen
-  },
-  FORGOT_PASS: {
-    name: 'ForgotPass',
-    component: ForgotPassword
-  },
-  HOME: {
-    name: 'Home',
-    component: HomeScreen
-  }
+  LOGIN: 'Login',
+  SIGNUP: 'Signup',
+  FORGOT_PASS: 'ForgotPass',
+  HOME: 'Home',
+  PROFILE: 'Profile',
 }
 
-// routes for navigation
-const routes = Object.values(AppRoute)
-
-export {AppRoute, routes};
+export { AppRoute };
