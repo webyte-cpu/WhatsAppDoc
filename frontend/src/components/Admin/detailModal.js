f5e68ef4937d7ea3f211d2e9cd589f7464d117bb
Dani <siroy.ayesha@gmail.com>
2021-04-03 00:37:18 +0800

    feat: add link new tab to PRC

--- frontend/src/components/Admin/detailModal.js
+++ frontend/src/components/Admin/detailModal.js
@@ -4,6 +4,7 @@ import {
     View,
     Image,
     Platform,
+    Linking,
 }
     from 'react-native';
 import {
@@ -14,6 +15,68 @@ import {
 }
     from '@ui-kitten/components';
 
+const openLink =(url) => { Platform.OS == 'web' ? window.open(url) : Linking.openURL(url) }
+
+const verifyLicenseBtn = (
+    <Button
+    status='danger'
+    style={{marginTop:20}}         
+    size='medium' 
+    appearance='outline'  
+    onPress={() => openLink('https://online1.prc.gov.ph/Verification')}>
+        Verify License Card at PRC site
+    </Button>
+)
+
+const DoctorDetails = ({ doctor, isShown, onHide }) => {
+
+    const Header = (props) => (
+        <View {...props}>
+            <Text category='h6'>{doctor.name}</Text>
+            <Text category='s2'>{doctor.specialization}</Text>
+        </View>
+    )
+
+    const Footer = (props) => (
+        doctor.verification == 'Verified' 
+        ? 
+            <></> 
+        :
+            <View {...props} style={styles.buttons}>
+                <Button 
+                style={styles.button}
+                onPress={onHide}>
+                    Deny
+                </Button>
+                <Button 
+                style={styles.button} 
+                onPress={onHide}>
+                    Verify
+                </Button>
+            </View>
+    )
+
+    return (
+        <Modal
+        style={styles.container}
+        visible={isShown}
+        backdropStyle={styles.backdrop}
+        onBackdropPress={onHide}>
+            <Card 
+            disabled={true} 
+            header={Header} 
+            footer={Footer}>
+                <Text category='s1'>License Card Information </Text>
+                <Image style={styles.image} source={{ uri: 'http://newstogov.com/wp-content/uploads/2019/10/prc1.jpg' }} />
+                <Text category='s1'>Birthdate: 
+                    <Text> {doctor.birthdate}</Text>
+                </Text>
+                {verifyLicenseBtn}
+            </Card>
+        </Modal>
+    );
+};
+
 const styles = StyleSheet.create({
     container: {
         width: 300,
@@ -22,7 +85,6 @@ const styles = StyleSheet.create({
                 width: 600,
             },
         }),
-
     },
     backdrop: {
         backgroundColor: 'rgba(0, 0, 0, 0.5)',
@@ -48,48 +110,5 @@ const styles = StyleSheet.create({
         }),
     },
 });
-
-const DoctorDetails = ({ doctor, isShown, onHide }) => {
-
-    const Header = (props) => (
-        <View {...props}>
-            <Text category='h6'>{doctor.name}</Text>
-            <Text category='s2'>{doctor.specialization}</Text>
-        </View>
-    )
-
-    const Footer = (props) => (
-        doctor.verification == 'Verified' 
-        ? 
-            <></> 
-        :
-            <View {...props} style={styles.buttons}>
-                <Button style={styles.button} onPress={onHide}>
-                    Deny
-            </Button>
-                <Button style={styles.button} onPress={onHide}>
-                    Verify
-            </Button>
-            </View>
-    )
-
-    return (
-        <Modal
-            style={styles.container}
-            visible={isShown}
-            backdropStyle={styles.backdrop}
-            onBackdropPress={onHide}>
-            <Card disabled={true} header={Header} footer={Footer}>
-                <Text category='s1'>License Card Information</Text>
-                <Image style={styles.image} source={{ uri: 'http://newstogov.com/wp-content/uploads/2019/10/prc1.jpg' }} />
-                <Text category='s1'>Birthdate: 
-                    <Text> {doctor.birthdate}</Text>
-                </Text>
-            </Card>
-        </Modal>
-    );
-};
-
-// https://online1.prc.gov.ph/Verification
 
 export default DoctorDetails
