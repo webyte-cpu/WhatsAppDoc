7dca5ae4a2c8d5e8c6eec4854eb23d949fdf3e3d
Aftertaste26 <20hardrock12@gmail.com>
2021-04-06 14:35:12 +0800

    feat: enum list added

---
+++ server/src/enum/typeDef.js
@@ -0,0 +1,47 @@
+import { gql } from "apollo-server-express";
+
+const enums = gql`
+  enum Role {
+    ADMIN
+    DOCTOR
+    PATIENT
+  }
+
+  enum Sex {
+    MALE
+    FEMALE
+  }
+
+  enum CivilStatus {
+    SINGLE
+    MARRIED
+    DIVORCED
+    WIDOWED
+  }
+
+  enum Status {
+    PENDING
+    IN_QUEUE
+    ON_GOING
+    DONE
+    CANCELLED
+  }
+
+  enum MedicalType {
+    ALLERGIES
+    SURGICAL
+    IMMUNIZATION
+    LABRATORY
+    GENERAL
+    OTHER
+  }
+
+  enum verificationStatus {
+    PENDING
+    VERIFIED
+    UNVERIFIED
+  }
+  
+`;
+
+export default enums
