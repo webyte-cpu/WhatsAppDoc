e3ad1f34136dae64016b1c740cebd6cb1abcccc7
Aftertaste26 <20hardrock12@gmail.com>
2021-04-08 19:34:05 +0800

    refactor: updated list of resolver

--- server/src/rootResolver/resolver.js
+++ server/src/rootResolver/resolver.js
@@ -1,13 +1,9 @@
 import { resolvers as scalarResolvers } from "graphql-scalars";
-
-import admin from "../admin/resolver.js";
 import user from "../user/resolver.js";
 import patient from "../patient/resolver.js";
 import doctor from "../doctor/resolver.js";
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
 
-const rootResolvers = [user, admin, patient, doctor];
+const rootResolvers = [user, patient, doctor];
 
 const resolvers = [...rootResolvers, scalarResolvers];
 export default resolvers;
