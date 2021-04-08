523c2aae3cda8c0b272be41cb94a7049429a8457
Aftertaste26 <20hardrock12@gmail.com>
2021-04-08 19:36:55 +0800

    refactor: updated user resolver type

--- server/src/user/resolver.js
+++ server/src/user/resolver.js
@@ -1,26 +1,31 @@
 import data from "../../db/sampleData.js";
 import user from "./user.js";
-
+import enums from "../enum/enum.js";
 const resolverMap = {
   User: {
     __resolveType(obj, context, info) {
-      if (obj.licenceNo && obj.isDoctor) {
-        return "Doctor";
+      console.log(obj.role);
+      switch (obj.role) {
+        case enums.role.ADMIN:
+          return "Admin";
+        case enums.role.DOCTOR:
+          return "Doctor";
+        case enums.role.PATIENT:
+          return "Patient";
+        default:
+          return null;
       }
-
-      if (obj.patientStatus || !obj.isDoctor) {
-        return "Patient";
-      }
-
-      return null;
     },
   },
 
   Query: {
     getUser: (obj, arg) => {
+      const { uid, sortBy, role } = obj;
+
       return data.user.filter((user) => user.uid === arg.uid);
     },
     getAllUser: (obj, arg) => {
+      /// return is user is admin
       return data.user;
     },
   },
