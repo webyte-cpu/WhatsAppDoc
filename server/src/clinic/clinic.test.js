fb2024fce44d555b4785be36b95584f47742f3fa
Michael Handumon <kmcaelian@gmail.com>
2021-04-16 18:17:49 +0800

    clinic query testing added

--- server/src/clinic/clinic.test.js
+++ server/src/clinic/clinic.test.js
@@ -7,6 +7,92 @@ const schemaCode = fs.readFileSync(
   "utf8"
 );
 
+const createClinic =`
+  mutation createClinic(
+    $address: String
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+    $sex: String
+    $phoneNumber: String
+    $weight: Float
+    $height: Float
+    $civilStatus: String
+    $nationality: String
+    $isClinic: Boolean
+    $licenceNo: String!
+    $experience: Int!
+    $rating: Int
+    $verificationStatus: VerificationStatus
+    $about: String
+    $bio: String
+  ) {
+    address: $address
+    firstName: $firstName,
+    lastName: $lastName,
+    email: $email,
+    password: $password,
+    sex: $sex,
+    phoneNumber: $phoneNumber,
+    weight: $weight,
+    height: $height,
+    civilStatus: $civilStatus,
+    nationality: $nationality,
+    isClinic: $isClinic,
+    licenceNo: $licenceNo,
+    experience: $experience,
+    rating: $rating,
+    verificationStatus: $verificationStatus
+    about: $about
+    bio: $bio
+  }
+`
+
+const updateClinic = `
+  mutation updateClinic(
+    $address: String!
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+    $sex: String
+    $phoneNumber: String
+    $weight: PositiveFloat
+    $height: PositiveFloat
+    $civilStatus: String
+    $nationality: String
+    $isClinic: Boolean
+    $licenceNo: String!
+    $experience: Int!
+    $rating: Int
+    $verificationStatus: VerificationStatus
+    $about: String
+    $bio: String
+  ) {
+    address: $addressInput
+    firstName: $firstName
+    lastName: $lastName
+    email: $email
+    password: $password
+    sex: $sex
+    phoneNumber: $phoneNumber
+    weight: $weight
+    height: $height
+    civilStatus: $civilStatus
+    nationality: $nationality
+    isClinic: $isClinic
+    licenceNo: $licenceNo
+    experience: $experience
+    rating: $rating
+    verificationStatus: $verificationStatus
+    about: $about
+    bio: $bio
+  }
+`
+
+
+
 describe("Test Queries and Mutations", () => {
   let tester;
 
@@ -14,5 +100,19 @@ describe("Test Queries and Mutations", () => {
     tester = new EasyGraphQLTester(schemaCode);
   });
 
-  
+  it("Should pass with a valid query", () => {
+      
+    const query = `
+    {
+      getClinic(uid: 3) {
+        name
+        roomNumber
+        address
+      }
+    }      
+    `;
+
+    tester.test(true, query);
+  });
+
 })
