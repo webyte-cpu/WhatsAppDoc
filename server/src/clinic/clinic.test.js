5bc20e96b7ea342152ab74ea30dc990820649457
Michael Handumon <kmcaelian@gmail.com>
2021-04-16 18:45:44 +0800

    clinic mutation testing added

--- server/src/clinic/clinic.test.js
+++ server/src/clinic/clinic.test.js
@@ -9,57 +9,63 @@ const schemaCode = fs.readFileSync(
 
 const createClinic =`
   mutation createClinic(
-    $address: String
-    $firstName: String!
-    $lastName: String!
-    $email: String!
-    $password: String!
-    $sex: String
-    $phoneNumber: String
-    $weight: Float
-    $height: Float
-    $civilStatus: String
-    $nationality: String
-    $isClinic: Boolean
-    $licenceNo: String!
-    $experience: Int!
-    $rating: Int
-    $verificationStatus: VerificationStatus
-    $about: String
+    $address: String!,
+    $firstName: String!,
+    $lastName: String!,
+    $email: String!,
+    $password: String!,
+    $sex: String!,
+    $phoneNumber: String,
+    $weight: Float,
+    $height: Float,
+    $civilStatus: String,
+    $nationality: String,
+    $isClinic: Boolean,
+    $licenceNo: String!,
+    $experience: Int!,
+    $rating: Int,
+    $verificationStatus: VerificationStatus,
+    $about: String,
     $bio: String
   ) {
-    address: $address
-    firstName: $firstName,
-    lastName: $lastName,
-    email: $email,
-    password: $password,
-    sex: $sex,
-    phoneNumber: $phoneNumber,
-    weight: $weight,
-    height: $height,
-    civilStatus: $civilStatus,
-    nationality: $nationality,
-    isClinic: $isClinic,
-    licenceNo: $licenceNo,
-    experience: $experience,
-    rating: $rating,
-    verificationStatus: $verificationStatus
-    about: $about
-    bio: $bio
+    createClinic(
+      address: $address,
+      firstName: $firstName,
+      lastName: $lastName,
+      email: $email,
+      password: $password,
+      sex: $sex,
+      phoneNumber: $phoneNumber,
+      weight: $weight,
+      height: $height,
+      civilStatus: $civilStatus,
+      nationality: $nationality,
+      isClinic: $isClinic,
+      licenceNo: $licenceNo,
+      experience: $experience,
+      rating: $rating,
+      verificationStatus: $verificationStatus,
+      about: $about,
+      bio: $bio
+    ) {
+      name
+      roomNumber
+      address
+    }
   }
 `
 
 const updateClinic = `
   mutation updateClinic(
-    $address: String!
-    $firstName: String!
-    $lastName: String!
-    $email: String!
-    $password: String!
-    $sex: String
-    $phoneNumber: String
-    $weight: PositiveFloat
-    $height: PositiveFloat
+    $address: String!,
+    $firstName: String!,
+    $lastName: String!,
+    $email: String!,
+    $password: String!,
+    $sex: String!,
+    $phoneNumber: String,
+    $weight: Float,
+    $height: Float,
     $civilStatus: String
     $nationality: String
     $isClinic: Boolean
@@ -70,28 +76,46 @@ const updateClinic = `
     $about: String
     $bio: String
   ) {
-    address: $addressInput
-    firstName: $firstName
-    lastName: $lastName
-    email: $email
-    password: $password
-    sex: $sex
-    phoneNumber: $phoneNumber
-    weight: $weight
-    height: $height
-    civilStatus: $civilStatus
-    nationality: $nationality
-    isClinic: $isClinic
-    licenceNo: $licenceNo
-    experience: $experience
-    rating: $rating
-    verificationStatus: $verificationStatus
-    about: $about
-    bio: $bio
+    updateClinic(
+      address: $address,
+      firstName: $firstName,
+      lastName: $lastName,
+      email: $email,
+      password: $password,
+      sex: $sex,
+      phoneNumber: $phoneNumber,
+      weight: $weight,
+      height: $height,
+      civilStatus: $civilStatus,
+      nationality: $nationality,
+      isClinic: $isClinic,
+      licenceNo: $licenceNo,
+      experience: $experience,
+      rating: $rating,
+      verificationStatus: $verificationStatus,
+      about: $about,
+      bio: $bio
+    ) {
+      name
+      roomNumber
+      address
+    }
   }
 `
 
-
+const deleteClinic = `
+  mutation deleteClinic(
+    $uid: ID!
+  ){
+    deleteClinic(
+      uid: $uid
+    ){
+      uid
+      name
+      roomNumber
+    }
+  }
+`
 
 describe("Test Queries and Mutations", () => {
   let tester;
@@ -114,5 +138,57 @@ describe("Test Queries and Mutations", () => {
 
     tester.test(true, query);
   });
+
+  it("Should pass if create query is correct", () => {
+    tester.test(true, createClinic, {
+      address: "Iloilo City",
+      firstName: "John",
+      lastName: "Doe",
+      email: "JohnDoe@gmail.com",
+      password: "password",
+      sex: "Male",
+      phoneNumber: "09312345831",
+      weight: 46.0,
+      height: 136.0,
+      civilStatus: "Single",
+      nationality: "Filipino",
+      isClinic: true,
+      licenceNo: "331-31-30",
+      experience: 2,
+      rating: 9,
+      verificationStatus: "VERIFIED",
+      about: "Test Doc",
+      bio: "Im a test doc"
+    })
+  })
+
+  it("Should pass if update query is correct", () => {
+    tester.test(true, updateClinic, {
+      address: "Iloilo City",
+      firstName: "John",
+      lastName: "Doe",
+      email: "JohnDoe@gmail.com",
+      password: "password",
+      sex: "Male",
+      phoneNumber: "09312345831",
+      weight: 46.0,
+      height: 136.0,
+      civilStatus: "Single",
+      nationality: "Filipino",
+      isClinic: true,
+      licenceNo: "331-31-30",
+      experience: 2,
+      rating: 9,
+      verificationStatus: "VERIFIED",
+      about: "Test Doc",
+      bio: "Im a test Doc"
+    })
+  })
+
+  it("Should pass if delete query is correct", () => {
+    tester.test(true, deleteClinic, {
+      uid: 3
+    })
+  })
 
 })
