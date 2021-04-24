075dfb831340938c57b2210b2ac0233c0d06244e
Wexxy <wexidalisay4@gmail.com>
2021-04-24 11:59:15 +0800

    frontend apollo client link

--- frontend/App.js
+++ frontend/App.js
@@ -6,16 +6,29 @@ import { default as theme } from './themes/custom-theme.json';
 import AppNavigator from './src/navigation/routes';
 import { AuthProvider } from './src/screens/auth/utils/authProvider';
 import { StatusBar } from 'expo-status-bar';
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+  fetchOptions: {
+    mode: 'no-cors',
+  }
+});
+
 const App = () => {
   return (
     <>
-      <AuthProvider>
-        <IconRegistry icons={EvaIconsPack} />
-        <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
-          <StatusBar translucent={true} />
-          <AppNavigator />
-        </ApplicationProvider>
-      </AuthProvider>
+      <ApolloProvider client={client}>
+        <AuthProvider>
+          <IconRegistry icons={EvaIconsPack} />
+          <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
+            <StatusBar translucent={true} />
+            <AppNavigator />
+          </ApplicationProvider>
+        </AuthProvider>
+      </ApolloProvider>
     </>
   );
 };
