daeb38487a1000a9217b09ffef3a5f8dfe0a6c56
Michael Handumon <kmcaelian@gmail.com>
2021-04-27 15:56:00 +0800

    working doctor mutation test

--- server/src/doctor/doctor.test.js
+++ server/src/doctor/doctor.test.js
@@ -2,18 +2,18 @@ import { createTestClient } from "apollo-server-testing";
 import { cleanDb, constructTestServer } from "../helpers/__utils.js";
 import { VERIFY_DOCTORS } from "./queries.js";
 
+const { server } = constructTestServer();
+    
+const { query } = createTestClient(server);
+
 describe("Mutations", () => {
   it("verify doctors", async () => {
-
-    const { server } = constructTestServer();
-    
-    const { query } = createTestClient(server);
 
     const res = await query({
       query: VERIFY_DOCTORS,
       variables: {
         uid:"123e4567-e89b-12d3-a456-426614174000", 
-        verificationStatus:VERIFIED
+        verificationStatus:'VERIFIED'
       }
     });
 
