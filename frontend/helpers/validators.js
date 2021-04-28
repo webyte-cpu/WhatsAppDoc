ef46a5d50b7dd9c34374eac8049021987d723fd1
edelynnm <edelynnm@gmail.com>
2021-04-28 14:14:25 +0800

    refactor: edit password err msg

--- frontend/helpers/validators.js
+++ frontend/helpers/validators.js
@@ -1,8 +1,9 @@
 export const VALID_PATTERNS = {
   PASSWORD: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-  ROLE: /^(PATIENT|DOCTOR)$/
-}
+  ROLE: /^(PATIENT|DOCTOR)$/,
+};
 
 export const ERR_MSG = {
-  PASSWORD: 'Must contain at least a number, an uppercase and lowercase letter, and at least 8 or more characters'
-}
+  PASSWORD: `Password must be 8 or more characters long and contain at least one of the following:
+number, uppercase letter, lowercase letter, and symbol.`,
+};
