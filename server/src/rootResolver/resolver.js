9732734a5cc56dd596be481ddd11ad2585c9cac5
edelynnm <edelynnm@gmail.com>
2021-05-05 02:19:36 +0800

    chore: temporary disable schedule schema

--- server/src/rootResolver/resolver.js
+++ server/src/rootResolver/resolver.js
@@ -3,11 +3,11 @@ import user from "../user/resolver.js";
 import patient from "../patient/resolver.js";
 import doctor from "../doctor/resolver.js";
 import address from "../address/resolver.js";
-import schedule from "../schedule/resolver.js";
+// import schedule from "../schedule/resolver.js";
 
 import { resolvers as customerScalarResolvers } from "../customScalar/index.js";
 
-const rootResolvers = [user, patient, doctor, address, schedule];
+const rootResolvers = [user, patient, doctor, address];
 
 const resolvers = [...rootResolvers, scalarResolvers, customerScalarResolvers];
 export default resolvers;
