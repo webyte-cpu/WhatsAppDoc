9da246ae54f38c7383264f5f5e9a264d2a7192fd
edelynnm <edelynnm@gmail.com>
2021-05-06 22:33:25 +0800

    refactor: edit profile icon avatar

--- frontend/src/components/drawer/drawerHeader.js
+++ frontend/src/components/drawer/drawerHeader.js
@@ -1,19 +1,22 @@
-import { Avatar, Divider, Text } from "@ui-kitten/components";
 import React from "react";
+import { Divider, Text } from "@ui-kitten/components";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useAuth } from "../../screens/auth/utils/authProvider";
+import ProfileIcon from "../profileIcon";
 
 const ProfileHeader = () => {
-  const auth = useAuth();
-  // const { fname, email } = auth.state.token;
+  const { appState } = useAuth();
+  const { firstName, lastName, email } = appState.user;
 
   return (
     <SafeAreaView>
       <View style={{ marginLeft: 10, marginVertical: 10 }}>
-        <Avatar size="giant" source={require("../../../assets/user.png")} />
-        {/* <Text category="h6"> {fname} </Text> */}
-        {/* <Text category="s1"> {email} </Text> */}
+        <ProfileIcon size="giant" firstName={firstName} lastName={lastName}/>
+        <Text category="h6" style={{ marginTop: 10 }}>
+          {firstName} {lastName}
+        </Text>
+        <Text category="c1">{email}</Text>
       </View>
       <Divider />
     </SafeAreaView>
