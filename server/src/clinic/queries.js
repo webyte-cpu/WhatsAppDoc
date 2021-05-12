00b1e8cfc44075ccddc5737c56797c3d707ca795
Michael Handumon <kmcaelian@gmail.com>
2021-05-12 12:43:03 +0800

    added clinic queries

---
+++ server/src/clinic/queries.js
@@ -0,0 +1,77 @@
+import { gql } from 'apollo-server-address';
+
+export const GET_CLINIC = gql`
+  query GetClinic($uid: UUID, $doctorUid: UUID) {
+    getClinic(uid: $uid, doctorUid: $doctorUid) {
+      uid
+      doctorClinicUid
+      name
+      roomNumber
+    }
+  }
+`;
+
+export const CREATE_CLINIC = gql`
+  mutation CreateClinic(
+    $doctorUid: UUID!,
+    $name: String!,
+    $roomNumber: String!,
+    $address: AddressInput!,
+    $minimumSchedulingNoticeMins: Int!,
+    $slotDurationInMins: Int!,
+    $consultationFee: Int
+  ) {
+    createClinic(
+      doctorUid: $doctorUid,
+      name: $name,
+      roomNumber: $roomNumber,
+      address: $address,
+      minimumSchedulingNoticeMins: $minimumSchedulingNoticeMins,
+      slotDurationInMins: $slotDurationInMins,
+      consultationFee: $consultationFee
+    ) {
+      uid
+      doctorClinicUid
+      name
+      roomNumber
+    }
+  }
+`;
+
+export const UPDATE_CLINIC = gql`
+  mutation UpdateClinic(
+    $uid: UUID!,
+    $name: String,
+    $roomNumber: String,
+    $address: AddressInput,
+    $minimumSchedulingNoticeMins: Int,
+    $slotDurationInMins: Int,
+    $consultationFee: Int
+  ) {
+    updateClinic(
+      uid: $uid,
+      name: $name,
+      roomNumber: $roomNumber,
+      address: $address,
+      minimumSchedulingNoticeMins: $minimumSchedulingNoticeMins,
+      slotDurationInMins: $slotDurationInMins,
+      consultationFee: $consultationFee
+    ) {
+      uid
+      doctorClinicUid
+      name
+      roomNumber
+    }
+  }
+` 
+
+export const DELETE_CLINIC = gql`
+  mutation DeleteClinic($uid: UUID!){
+    deleteClinic(uid: $uid) {
+      uid
+      doctorClinicUid
+      name
+      roomNumber
+    }
+  }
+`
