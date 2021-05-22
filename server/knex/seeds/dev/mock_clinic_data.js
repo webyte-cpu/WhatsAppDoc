ee8fe3691d16b7cad02d8647ed0331363af6e0b7
Michael Handumon <kmcaelian@gmail.com>
2021-05-22 21:00:40 +0800

    schedule testing debug & address seeds

--- server/knex/seeds/dev/mock_clinic_data.js
+++ server/knex/seeds/dev/mock_clinic_data.js
@@ -1,29 +1,31 @@
 const seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('clinic')
+  return knex('clinics')
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex('clinic').insert([
+      return knex('clinics').insert([
         {
-          uid: '027ae0ac-05d6-43bf-818e-a76607b7f976',
-          doctorClinicUid: '0afdaddf-536e-4b67-8617-52783e64f6b5',
-          name: 'Test Clinic',
-          roomNumber: '3',
-          address: "Iloilo City",
-          minimumSchedulingNoticeMins: 4,
-          slotDurationInMins: 3,
-          consultationFee: 500
+          clinic_uid: '027ae0ac-05d6-43bf-818e-a76607b7f976',
+          address_uid: '0afdaddf-536e-4b67-8617-52783e64f6b5',
+          clinic_name: 'Test Clinic',
+          // roomNumber: '3',
+          // address: "Iloilo City",
+          // minimumSchedulingNoticeMins: 4,
+          // slotDurationInMins: 3,
+          // consultationFee: 500
+          clinic_room_no: '3'
         },
         {
-          uid: 'f9609c66-d0a1-48c6-b8bd-3a1a975986d4',
-          doctorClinicUid: 'd88e25f9-75fc-463b-8c06-4edb67195a6b',
-          name: 'Test Clinic',
-          roomNumber: '5',
-          address: "Iloilo City",
-          minimumSchedulingNoticeMins: 4,
-          slotDurationInMins: 3,
-          consultationFee: 500
+          clinic_uid: 'f9609c66-d0a1-48c6-b8bd-3a1a975986d4',
+          address_uid: 'd88e25f9-75fc-463b-8c06-4edb67195a6b',
+          clinic_name: 'Test Clinic',
+          // roomNumber: '5',
+          // address: "Iloilo City",
+          // minimumSchedulingNoticeMins: 4,
+          // slotDurationInMins: 3,
+          // consultationFee: 500
+          clinic_room_no: '5'
         },
       ]);
     });
--- server/knex/seeds/dev/mock_schedule_data.js
+++ server/knex/seeds/dev/mock_schedule_data.js
@@ -10,14 +10,14 @@ const seed = function (knex) {
           doctorClinicUid: '0353121f-eeed-4687-8676-f788d3e9c8e6',
           start_time: "10:30:00Z",
           end_time: "11:30:00Z",
-          day_of_week: "4"
+          day_of_week: JSON.stringify([4])
         },
         {
           uid: '9733cbae-dc7d-451c-af9c-ed7618ca9029',
           doctorClinicUid: '3a238cae-f2af-4d4b-bb34-252215b5272e',
           start_time: "8:00:00Z",
           end_time: "9:00:00Z",
-          day_of_week: "1"
+          day_of_week: JSON.stringify([1, 3])
         },
       ]);
     });
