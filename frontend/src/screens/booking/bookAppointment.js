43f90eba979ca2d463bbac0c17911f13c26482a1
Dani <siroy.ayesha@gmail.com>
2021-05-27 15:00:36 +0800

    feat: add booked doctor description and styling of components

--- frontend/src/screens/booking/bookAppointment.js
+++ frontend/src/screens/booking/bookAppointment.js
@@ -1,32 +1,37 @@
 import React, { useState } from "react";
-import { View, ScrollView } from 'react-native'
-import { Calendar, Select, IndexPath, SelectItem, Divider, Button, Input, Text, Icon } from '@ui-kitten/components'
+import { View, ScrollView, StyleSheet } from 'react-native'
+import { Calendar, Select, IndexPath, SelectItem, Divider, Button, Input, Text} from '@ui-kitten/components'
 import SuccessModal from './successModal'
+import { RenderDoctor } from  '../search/doctors/nearbyDoctors'
 
-// const Bookmark = (...props) => { return <Icon {...props} name='bookmark' /> }
 
-const prefix = (prefix) => {
-    return <Text category="s2" style={{ color: 'gray' }}>{prefix}</Text>;
-};
-
-const InfoScreen = () => {
+const InfoScreen = ({doctor}) => {
+    const prefix = (prefix) => <Text category="s2" style={{ color: 'gray' }}>{prefix}</Text>;
+    
     return (
         <View>
+            <RenderDoctor disabled={true} item={doctor} />
             <Divider />
-            <Input style={{ backgroundColor: 'white', marginVertical: 5 }} disabled={true} label='CLINIC' />
+            <Input   
+            style={styles.inputContainer}
+            textStyle={styles.inputValue}
+            disabled={true} 
+            label='CLINIC'
+            value={`Doctor's Medical Hospital`} />
             <Input
-                style={{ backgroundColor: 'white', marginVertical: 5, color: 'black' }}
-                disabled={true}
-                label='CONSULTATION FEE'
-                value={'500.00'}
-                textStyle={{ color: 'black', fontWeight: 'bold' }}
-                accessoryLeft={() => prefix('Php')} />
+            style={styles.inputContainer}
+            disabled={true}
+            label='CONSULTATION FEE'
+            value='500.00'
+            textStyle={styles.inputValue}
+            accessoryLeft={() => prefix('Php')} />
         </View>
     )
-}
+};
 
 const BookingScreen = () => {
-    const dummyData = ['7:00 - 7:30', '7:30 - 8:00', '8:00 - 8:30', '8:30 - 9:00', '9:00 - 9:30']
+    const timeData = ['7:00 - 7:30', '7:30 - 8:00', '8:00 - 8:30', '8:30 - 9:00', '9:00 - 9:30']
+    const doctorData = { firstName: 'Alexis', lastName:'Dalisay', specialization: 'Physician', rating: 5, exp: 2 }
     const [visible, setVisible] = useState(false);
 
     const handleClose = () => setVisible(false);
@@ -36,9 +41,7 @@ const BookingScreen = () => {
         const [selectedDate, setSelectedDate] = React.useState(new Date());
         return (
             <>
-                <Text category='h6'>
-                    Selected date: {selectedDate.toLocaleDateString()}
-                </Text>
+                {/* <Text category='h6'> Selected date: {selectedDate.toLocaleDateString()} </Text> */}
                 <Calendar
                     date={selectedDate}
                     onSelect={nextDate => setSelectedDate(nextDate)}
@@ -51,35 +54,50 @@ const BookingScreen = () => {
         const [selectedIndex, setSelectedIndex] = useState(new IndexPath(0));
         // const [selectedTime, setSelectedTime] = useState('')
 
-        const displayValue = dummyData[selectedIndex.row];
+        const displayValue = timeData[selectedIndex.row];
         const renderOption = (title) => <SelectItem title={title} />
 
         return (
             <View style={{marginVertical:10}}>
                 <Select
-                    label='TIME'
-                    value={displayValue}
-                    selectedIndex={selectedIndex}
-                    onSelect={index => setSelectedIndex(index)}
- 
+                label='TIME'
+                value={displayValue}
+                selectedIndex={selectedIndex}
+                onSelect={index => setSelectedIndex(index)}
                 >
-                    {dummyData.map(renderOption)}
+                    {timeData.map(renderOption)}
                 </Select>
             </View>
         )
     }
 
     return (
-        <ScrollView style={{ padding: 20, backgroundColor: 'white', height: '100%' }}>
-            <InfoScreen />
+        <ScrollView style={styles.container}>
+            <InfoScreen doctor={doctorData} />
             <SelectTime />
             <CalendarScreen />
-            <Button onPress={handleShow}>
+            <Button style={{marginVertical:10}} onPress={handleShow}>
                 Book Appointment
              </Button>
             <SuccessModal isShown={visible} onHide={handleClose} />
         </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        padding: 20, 
+        backgroundColor: 'white', 
+        height: '100%' 
+    },
+    inputValue: {
+        color: 'black', 
+        fontWeight: 'bold' 
+    },
+    inputContainer:{
+        backgroundColor: 'white', 
+        marginVertical: 5
+    }
+})
 
 export default BookingScreen;
