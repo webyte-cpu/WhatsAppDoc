b0345bf6a5a777140345e64a1fc84a2d03069727
Aftertaste26 <20hardrock12@gmail.com>
2021-05-28 15:12:38 +0800

    feat: implemented dataloader

--- server/src/schedule/resolver.js
+++ server/src/schedule/resolver.js
@@ -3,14 +3,31 @@ import __ from "lodash";
 
 const resolverMap = {
   Query: {
-    // accepts specific schedule UID || doctorClinicUidto get all schedules
-    getSchedule: async (obj, arg) => schedule.get(arg), 
+    getSchedule: async (obj, arg) => schedule.get(arg.uid),
+    getAllSchedule: async (obj, arg) => schedule.getAll(),
   },
   Mutation: {
-    upsertSchedule: (obj, arg) => schedule.upsert(arg.data),
-    createSchedule: (obj, arg) => schedule.create(arg.data),
-    updateSchedule: (obj, arg) => schedule.update(arg),
-    deleteSchedule: (obj, arg) => schedule.remove(arg.uid),
+    upsertSchedule: async (obj, arg, { loader }) => {
+      const response = await schedule.upsert(arg);
+      console.log(loader);
+      loader?.schedule?.clear(arg.doctorClinicUid);
+      return response;
+    },
+    createSchedule: async (obj, arg, { loader }) => {
+      const response = await schedule.create(arg);
+      loader?.schedule?.clear(arg.doctorClinicUid);
+      return response;
+    },
+    updateSchedule: async (obj, arg, { loader }) => {
+      const response = await schedule.update(arg);
+      loader?.schedule?.clear(arg.doctorClinicUid);
+      return response;
+    },
+    deleteSchedule: async (obj, arg, { loader }) => {
+      const response = schedule.remove(arg.uid);
+      loader?.schedule?.clear(arg.doctorClinicUid);
+      return response;
+    },
   },
 };
 
