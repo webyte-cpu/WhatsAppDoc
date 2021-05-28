8d7ec9f694a83aa67970fdc0b6e6346695171c58
Aftertaste26 <20hardrock12@gmail.com>
2021-05-28 14:39:02 +0800

    feat: added find function

--- server/src/address/model.js
+++ server/src/address/model.js
@@ -1,8 +1,9 @@
 import objectFilter from "../helpers/objectFilter.js";
+import { ApolloError } from "apollo-server-express";
+import findModel from "../helpers/find.js";
 import { v4 as uuidV4 } from "uuid";
 import pg from "../../db/index.js";
 import __ from "lodash";
-import { ApolloError } from "apollo-server-express";
 
 const fromDb = (data) => ({
   uid: data.address_uid,
@@ -36,38 +37,43 @@ const create = async (addressData, knex = pg) => {
 
 const update = async (addressData, knex = pg) => {
   try {
-    console.log(addressData,'addDATA')
-  const dbResponse = await knex("addresses")
-    .where({ address_uid: addressData.uid })
-    .update(
-      objectFilter({
-        address: addressData.address,
-        address_city: addressData.city,
-        address_province: addressData.province,
-        address_zip_code: addressData.zipCode,
-        address_country: addressData.country,
-        address_coordinates: addressData.coordinates,
-      })
-    )
-    .returning("*")
+    console.log(addressData, "addDATA");
+    const dbResponse = await knex("addresses")
+      .where({ address_uid: addressData.uid })
+      .update(
+        objectFilter({
+          address: addressData.address,
+          address_city: addressData.city,
+          address_province: addressData.province,
+          address_zip_code: addressData.zipCode,
+          address_country: addressData.country,
+          address_coordinates: addressData.coordinates,
+        })
+      )
+      .returning("*");
 
-    console.log(dbResponse, 'DB')
+    console.log(dbResponse, "DB");
 
-  return fromDb(__.first(dbResponse));
-    
+    return fromDb(__.first(dbResponse));
   } catch (error) {
-    console.log(error)
-    throw new ApolloError(error)
+    console.log(error);
+    throw new ApolloError(error);
   }
-  
 };
 
 const get = async (uid, knex = pg) => {
   const dbResponse = await knex
     .select("*")
     .from("addresses")
-    .where(objectFilter({ address_uid: uid }));
+    .where({ address_uid: uid });
 
+  if (!__.isEmpty(dbResponse)) {
+    return fromDb(__.first(dbResponse));
+  }
+};
+
+const getAll = async (knex = pg) => {
+  const dbResponse = await knex.select("*").from("addresses");
   return dbResponse.map(fromDb);
 };
 
@@ -79,4 +85,6 @@ const remove = async (uid, knex = pg) => {
   return fromDb(__.first(dbResponse));
 };
 
-export default { create, update, get, remove };
+const find = findModel("addresses", fromDb, toDb, pg);
+
+export default { create, update, get, remove, getAll, find };
--- server/src/address/resolver.js
+++ server/src/address/resolver.js
@@ -3,6 +3,7 @@ import address from "./model.js";
 export default {
   Query: {
     getAddress: (obj, arg) => address.get(arg.uid),
+    getAllAddresses: (obj, arg) => address.getAll(),
   },
   Mutation: {
     createAddress: (obj, arg) => address.create(arg),
