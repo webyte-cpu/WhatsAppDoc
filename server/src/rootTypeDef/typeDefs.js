6b1a33eb0ffc7473768e603b9c58160f8d1c97f9
Aftertaste26 <20hardrock12@gmail.com>
2021-05-28 15:40:40 +0800

    chore: removed 'appointment' has already been declared

--- server/src/rootTypeDef/typeDefs.js
+++ server/src/rootTypeDef/typeDefs.js
@@ -10,7 +10,6 @@ import signIn from "../signIn/typeDef.js";
 import doctor from "../doctor/typeDef.js";
 import clinic from "../clinic/typeDef.js";
 import user from "../user/typeDef.js";
-import appointment from "../appointment/typeDef.js";
 
 const rootTypeDefs = gql`
   type Query {
