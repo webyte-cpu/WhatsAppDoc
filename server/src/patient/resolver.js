e5c3c726b7a685f103554f1e02f1a52251194402
Aftertaste26 <20hardrock12@gmail.com>
2021-05-30 00:47:59 +0800

    refactor: refactored and added some restrictions

--- server/src/patient/resolver.js
+++ server/src/patient/resolver.js
@@ -2,14 +2,85 @@ import patient from "./model.js";
 import __ from "lodash";
 
 const resolverMap = {
+  Patient: {
+    address: async (patient, arg, { loader }) => {
+      if (__.isNull(patient.addressUid)) {
+        return null;
+      }
+      try {
+        const response = await loader.address.load(patient.addressUid);
+        return __.first(response);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    appointment: async (patient, arg, { loader }) => {
+      if (__.isNull(patient.patientUid)) {
+        return null;
+      }
+      try {
+        return loader.appointment.groupBy("patientUid").load(patient.uid);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
   Query: {
-    getPatient: (obj, arg) => patient.get(arg.uid),
+    getPatient: (obj, arg) => {
+      if (__.isEmpty(user)) {
+        throw new AuthenticationError("No authorization header found");
+      }
+      return patient.get(arg.uid);
+    },
     getAllPatients: (obj, arg) => patient.getAll(),
   },
   Mutation: {
-    createPatient: (obj, arg) => patient.create(arg),
-    updatePatient: (obj, arg) => patient.update(arg),
-    deletePatient: (obj, arg) => patient.remove(arg.uid),
+    createPatient: (obj, arg, { user }) => {
+      if (__.isEmpty(user)) {
+        throw new AuthenticationError("No authorization header found");
+      }
+
+      if (![enums.role.ADMIN].includes(user.role)) {
+        throw new ForbiddenError("Not authorize to create doctor");
+      }
+
+      try {
+        const response = patient.create(arg);
+        return response;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    updatePatient: (obj, doctorData, { user }) => {
+      if (__.isEmpty(user)) {
+        throw new AuthenticationError("No authorization header found");
+      }
+
+      if (![enums.role.ADMIN, enums.role.DOCTOR].includes(user.role)) {
+        throw new ForbiddenError("Not authorize to update doctor");
+      }
+      try {
+        const response = patient.update(arg);
+        return response;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    deletePatient: (obj, doctorData, { user }) => {
+      if (__.isEmpty(user)) {
+        throw new AuthenticationError("No authorization header found");
+      }
+
+      if (![enums.role.ADMIN, enums.role.DOCTOR].includes(user.role)) {
+        throw new ForbiddenError("Not authorize to delete doctor");
+      }
+      try {
+        const response = patient.remove(arg.uid);
+        return response;
+      } catch (error) {
+        console.error(error);
+      }
+    },
   },
   //Subscription: {},
 };
