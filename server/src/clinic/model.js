47341d8ec4338358412a38ed92ec43d0b7c8f989
edelynnm <edelynnm@gmail.com>
2021-05-30 14:51:22 +0800

    chore: revert to old state

--- server/src/clinic/model.js
+++ server/src/clinic/model.js
@@ -134,17 +134,19 @@ const update = (clinicData, knex = pg) =>
       throw new ApolloError(error.detail);
     }
   });
-const get = async (doctorUid, knex = pg) => {
+const get = async ({uid, doctorUid}, knex = pg) => {
   try {
     const dbResponse = await knex
       .select("*")
       .from("clinics")
-      .where("doctor_uid", doctorUid)
+      .where(objectFilter({"clinics.clinic_uid": uid, doctor_uid: doctorUid}))
+      .orderBy("clinic_name")
       .innerJoin(
         "doctor_clinics",
         "doctor_clinics.clinic_uid",
         "clinics.clinic_uid"
       );
+      console.log(dbResponse)
     return dbResponse.map(fromDb);
   } catch (error) {
     console.log(error);
