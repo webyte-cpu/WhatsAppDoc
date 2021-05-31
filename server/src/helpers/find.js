8b9b392e457d39644f6ff648b550a5edb757ab8c
Aftertaste26 <20hardrock12@gmail.com>
2021-05-31 11:19:04 +0800

    refactor: added key check for specified table for the column

--- server/src/helpers/find.js
+++ server/src/helpers/find.js
@@ -3,7 +3,22 @@ import __ from "lodash";
 export default (table, mapFromDb, mapToDb, knex, builder) =>
   async (obj, trx = knex) => {
     try {
-      const pair = __.toPairs(objectFilter(mapToDb(obj)));
+      const checkKeys = (obj, mapToDb) =>
+        __.keysIn(obj).reduce((acc, key) => {
+          if (key.includes(".")) {
+            const splitKey = __.split(key, ".");
+            const table = splitKey[0];
+            const column = splitKey[1];
+            const columnToDb = (column) =>
+              __.flow([mapToDb, objectFilter, __.keysIn])({ [column]: true });
+            const newColumn = `${table}.${columnToDb(column)}`;
+            return { ...acc, [newColumn]: obj[key] };
+          }
+
+          return { ...acc, ...objectFilter(mapToDb({ [key]: obj[key] })) };
+        }, {});
+
+      const pair = __.toPairs(checkKeys(obj, mapToDb));
       const zip = __.zip(...pair);
       const column = zip[0];
       const values = __.zip(...zip[1]);
