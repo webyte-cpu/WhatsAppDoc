d5faa1b1db2507bed49f0ce961ab9c17a47cdb58
edelynnm <edelynnm@gmail.com>
2021-06-06 03:15:09 +0800

    chore: add conditional menu btn render

--- frontend/src/navigation/navigatorStacks/profileStack.js
+++ frontend/src/navigation/navigatorStacks/profileStack.js
@@ -4,26 +4,31 @@ import DrawerMenuBtn from '../../components/drawer/drawerBtn';
 import ProfilePage from '../../screens/profile/profilePage';
 import MedicalRecordStackScreen from './medicalRecordStack';
 import { AppRoute } from '../app-routes';
+import { useWindowDimensions } from 'react-native';
+import breakpoints from '../../utils/breakpoints';
 
 const ProfileStack = createStackNavigator();
 
-const ProfileStackScreen = (props) => (
+const ProfileStackScreen = (props) => {
+  const dimensions = useWindowDimensions();
+  
+  return (
   <ProfileStack.Navigator>
     <ProfileStack.Screen
       name={AppRoute.PROFILE}
       component={ProfilePage}
       options={{
-        headerLeft: () => (
-          <DrawerMenuBtn props={props} />
-        )
+        headerLeft: () => {
+          if(dimensions.width < breakpoints.lg) return <DrawerMenuBtn props={props} />
+        },
       }}
     />
-    <ProfileStack.Screen
+    {/* <ProfileStack.Screen
       name={AppRoute.MEDICAL_REC}
       component={MedicalRecordStackScreen}
       options={{headerShown:false}}
-    />
+    /> */}
   </ProfileStack.Navigator>
-)
+)}
 
 export default ProfileStackScreen;
