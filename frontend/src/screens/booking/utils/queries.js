dbcafddc9f53d6c39094c0447d291e73c493f69a
edelynnm <edelynnm@gmail.com>
2021-06-09 13:46:22 +0800

    feat: add create_appointment mutation

---
+++ frontend/src/screens/booking/utils/queries.js
@@ -0,0 +1,14 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_APPOINTMENT = gql`
+  mutation createAppointment(
+    $doctorClinicUid: UUID!
+    $schedule: DateTime!
+  ) {
+    createAppointment(
+      doctorClinicUid: $doctorClinicUid, 
+      dateTime: $schedule) {
+      uid
+    }
+  }
+`
