d80d665c307d84db0b9b5a1d6b8490be28e103dc
edelynnm <edelynnm@gmail.com>
2021-06-09 12:22:35 +0800

    chore: prettify

--- frontend/src/screens/search/searchPage.js
+++ frontend/src/screens/search/searchPage.js
@@ -1,92 +1,109 @@
-import React, { useState, useEffect } from 'react';
-import { View } from 'react-native';
-import { Divider, Text } from '@ui-kitten/components';
-import Searchbar from './search';
-import { useQuery } from '@apollo/client'
-import { GET_ALL_DOCTORS } from './utils/queries';
-import LoadingScreen from '../../components/loadingScreen';
-import { FILTER_BY } from './utils/filters';
-import enums from '../../../helpers/enums';
-import { getByLocationString, getByName, getNearbyDoctors } from './utils/searchDoctors';
-import { DoctorsList } from './doctors/doctorsList';
-import customStyle from '../../../themes/styles';
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
+import { Divider, Text } from "@ui-kitten/components";
+import Searchbar from "./search";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_DOCTORS } from "./utils/queries";
+import LoadingScreen from "../../components/loadingScreen";
+import { FILTER_BY } from "./utils/filters";
+import enums from "../../../helpers/enums";
+import {
+  getByLocationString,
+  getByName,
+  getNearbyDoctors,
+} from "./utils/searchDoctors";
+import { DoctorsList } from "./doctors/doctorsList";
+import customStyle from "../../../themes/styles";
 
 const SearchPage = ({ navigation, route }) => {
-  const [ verifiedDoctors, setVerifiedDoctors ] = useState([])
-  const { loading: queryLoading, error, data } = useQuery(GET_ALL_DOCTORS, {
+  const [verifiedDoctors, setVerifiedDoctors] = useState([]);
+  const {
+    loading: queryLoading,
+    error,
+    data,
+  } = useQuery(GET_ALL_DOCTORS, {
     onCompleted: (data) => {
-      setVerifiedDoctors(data.results.filter((doctor) => doctor.verificationStatus === enums.verificationStatus.VERIFIED))
+      setVerifiedDoctors(
+        data.results.filter(
+          (doctor) =>
+            doctor.verificationStatus === enums.verificationStatus.VERIFIED
+        )
+      );
     },
-    pollInterval: 1000
-  })
-  const [searchResults, setSearchResults] = useState([])
-  const [loading, setLoading] = useState(true)
+    pollInterval: 1000,
+  });
+  const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if(verifiedDoctors) {
-      inputSearch(route.params)
+    if (verifiedDoctors) {
+      inputSearch(route.params);
     }
-  }, [verifiedDoctors])
+  }, [verifiedDoctors]);
 
-  if(loading || queryLoading) {
-    return <LoadingScreen />
+  if (loading || queryLoading) {
+    return <LoadingScreen />;
   }
 
-  if(error) {
-    console.error(error)
-    return null
+  if (error) {
+    console.error(error);
+    return null;
   }
 
-  function searchDoctor (query, filter, verifiedDoctors) {
-    let filteredDoctors = []; 
+  function searchDoctor(query, filter, verifiedDoctors) {
+    let filteredDoctors = [];
 
-    if(verifiedDoctors.length > 0){
-      
-      switch(filter) {
-        case FILTER_BY.Name: 
-          filteredDoctors = getByName(query.query, verifiedDoctors)
+    if (verifiedDoctors.length > 0) {
+      switch (filter) {
+        case FILTER_BY.Name:
+          filteredDoctors = getByName(query.query, verifiedDoctors);
           break;
-        case FILTER_BY.Location: 
-          if(query.query.coords) { // USE LOCATION
-            filteredDoctors = getNearbyDoctors(query.query.coords, verifiedDoctors, 3)
-            return
+        case FILTER_BY.Location:
+          if (query.query.coords) {
+            // USE LOCATION
+            filteredDoctors = getNearbyDoctors( query.query.coords, verifiedDoctors, 3 );
+            return;
           }
 
-          filteredDoctors = getByLocationString(query.query, verifiedDoctors)
+          filteredDoctors = getByLocationString(query.query, verifiedDoctors);
           break;
         default:
-          console.error('INVALID FILTER')
+          console.error("INVALID FILTER");
       }
-
     }
 
-    return filteredDoctors
+    return filteredDoctors;
   }
 
-  function inputSearch (input) {
+  function inputSearch(input) {
     let matchDoctors;
 
-    if(input.query.query === '') {
-      matchDoctors = verifiedDoctors
+    if (input.query.query === "") {
+      matchDoctors = verifiedDoctors;
     } else {
-      matchDoctors = searchDoctor(input.query,input.filter, verifiedDoctors)
+      matchDoctors = searchDoctor(input.query, input.filter, verifiedDoctors);
     }
 
-    setSearchResults(matchDoctors)
-    setLoading(false)
+    setSearchResults(matchDoctors);
+    setLoading(false);
   }
 
   return (
     <View>
-      <View style={[customStyle.contentFill, {paddingBottom: 20 }]}>
-        <Searchbar inputFilter={route.params.filter} inputQuery={route.params.query} route={route} inputSearch={inputSearch}/>
+      <View style={[customStyle.contentFill, { paddingBottom: 20 }]}>
+        <Searchbar
+          inputFilter={route.params.filter}
+          inputQuery={route.params.query}
+          route={route}
+          inputSearch={inputSearch}
+        />
       </View>
-      <Divider/>
-      <View style={customStyle.contentFill}>
-      <DoctorsList searchResults={searchResults} navigation={navigation}/>
+      <Divider />
+      <View style={[customStyle.contentFill, {paddingHorizontal: 10}]}>
+        <DoctorsList searchResults={searchResults} navigation={navigation} />
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
