204239398b9b49fb6abe7d556fffd327c38cf74e
Wexxy <wexidalisay4@gmail.com>
2021-06-10 17:21:54 +0800

    perf(request page): add minimum scheduling notice

--- frontend/src/screens/request/request-patient.js
+++ frontend/src/screens/request/request-patient.js
@@ -150,7 +150,7 @@ const RequestPatientPage = ({ navigation }) => {
     }
 
     const cancelingHandler = (item) => {
-        const minimum = 3
+        const minimum = item.minimumSchedulingNoticeMins/60
         const getDiffinHrs = Difference(item.dateTime)
 
         if (getDiffinHrs < minimum) {
